ac32da71d44c31604f1ef411d1992ca23246ae7f
Dmitry Kostromin <d.kostromin@corp.vk.com>
2021-01-21 17:03:46 +0300

    Add git interactive rebase helper

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,13 @@
+ROOT=$(dirname "$(readlink -f "$0")")
+
+source "$ROOT/helpers.sh"
+
 alias gitbrd='git branch -d $(git branch | fzf -m)'
 alias gitco='git checkout $(git branch | fzf)'
 alias gitpud='git push origin :$(git branch | fzf)'
+alias gitrei='git-rebase-i'
+
+function git-rebase-i {
+    git rebase -i $(git log --oneline --decorate --graph | fzf | pick-col 2)
+}
+
---
+++ helpers.sh
@@ -0,0 +1,4 @@
+function pick-col {
+    awk '{print $'"$1"'}'
+}
+
