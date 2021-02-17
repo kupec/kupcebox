03ebf03361afe614ffe5c5170d6af43591eb1a7b
Dmitry Kostromin <d.kostromin@corp.vk.com>
2021-02-17 14:36:18 +0300

    Add tmux_aliases with tclear

--- all.sh
+++ all.sh
@@ -3,3 +3,4 @@ QP_ROOT=$(dirname "$(readlink -f "$0")")
 export PATH="$PATH:$QP_ROOT/bin"
 
 source "$QP_ROOT/git_aliases.sh"
+source "$QP_ROOT/tmux_aliases.sh"
---
+++ tmux_aliases.sh
@@ -0,0 +1,4 @@
+QP_ROOT=$(dirname "$(readlink -f "$0")")
+source "$QP_ROOT/include/helpers.sh"
+
+alias tclear='clear; tmux clear-history'
